796dfad8ab3004d6a6f1c3abb5e5b8488a246119
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2024-05-02 01:52:59 +0900

    chore: set up Rollup

---
+++ packages/chibivite/rollup.config.ts
@@ -0,0 +1,21 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  input: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    file: path.resolve(__dirname, 'dist/index.mjs'),
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript({
+      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+      sourceMap: true,
+    }),
+  ],
+})
