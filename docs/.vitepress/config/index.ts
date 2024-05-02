1a60bc3856d96002fc1f77a86bdd2ac3dd85292c
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2024-05-03 08:10:21 +0900

    chore: enable type inference of VitePress config

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,31 +1,34 @@
 import { withMermaid } from 'vitepress-plugin-mermaid'
 import { enConfig } from './en'
 import { jaConfig, jaSearchConfig } from './ja'
+import { defineConfig } from 'vitepress'
 
 export default withMermaid({
-  srcDir: 'content',
-  base: '/chibivite/',
-  title: 'chibivite',
-  lastUpdated: true,
-  themeConfig: {
-    externalLinkIcon: true,
-    socialLinks: [
-      { icon: 'twitter', link: 'https://twitter.com/NozomuIkuta' },
-      { icon: 'github', link: 'https://github.com/NozomuIkuta/chibivite' },
-    ],
-    search: {
-      provider: 'local',
-      options: {
-        locales: {
-          ...jaSearchConfig,
+  ...defineConfig({
+    srcDir: 'content',
+    base: '/chibivite/',
+    title: 'chibivite',
+    lastUpdated: true,
+    themeConfig: {
+      externalLinkIcon: true,
+      socialLinks: [
+        { icon: 'twitter', link: 'https://twitter.com/NozomuIkuta' },
+        { icon: 'github', link: 'https://github.com/NozomuIkuta/chibivite' },
+      ],
+      search: {
+        provider: 'local',
+        options: {
+          locales: {
+            ...jaSearchConfig,
+          },
         },
       },
     },
-  },
-  locales: {
-    root: { label: 'English', ...enConfig },
-    ja: { label: '日本語', ...jaConfig },
-  },
+    locales: {
+      root: { label: 'English', ...enConfig },
+      ja: { label: '日本語', ...jaConfig },
+    },
+  }),
   mermaid: {},
   mermaidPlugin: {},
 })
