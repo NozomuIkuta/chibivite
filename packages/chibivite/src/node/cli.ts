c16291fca84ad36c163a250e9ce4c09e33e59d7b
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2024-05-06 02:29:06 +0900

    chore: refine CLI code

--- packages/chibivite/src/node/cli.ts
+++ packages/chibivite/src/node/cli.ts
@@ -2,11 +2,9 @@ import { cac } from 'cac'
 
 const cli = cac('chibivite')
 
-cli.command('[root]', 'start dev server').action(async (root: string) => {
-  console.log(`Hello, chibivite at ${root}!`)
-  const { createServer } = await import('./server')
-
+cli.command('', 'start dev server').action(async () => {
   try {
+    const { createServer } = await import('./server')
     const server = await createServer()
     await server.listen()
   } catch (e) {
