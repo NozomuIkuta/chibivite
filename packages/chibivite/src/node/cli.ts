9001cc2da2052df495291b54fd7810804f2af2da
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2024-05-06 02:42:51 +0900

    chore: show message after server started

--- packages/chibivite/src/node/cli.ts
+++ packages/chibivite/src/node/cli.ts
@@ -7,6 +7,7 @@ cli.command('', 'start dev server').action(async () => {
     const { createServer } = await import('./server')
     const server = await createServer()
     await server.listen()
+    console.log('Listening...')
   } catch (e) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     console.error(`error when starting dev server:\n${(e as any).stack}`)
